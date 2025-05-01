393ca3240860fbb7768f5e6008471d7f35cd24c3
Harish <harishgokul01@gmail.com>
2025-05-01 16:14:10 -0700

    Update explorer.tsx

--- src/components/graphs/explorer.tsx
+++ src/components/graphs/explorer.tsx
@@ -120,7 +120,7 @@ export function Explorer() {
               <DatabaseCard loading={loading} data={configData} />
               <ChainInfoCard loading={loading} data={configData} />
             </div>
-            <div className="p-4">
+            <div className="gap-4">
               <MiscellaneousDataCard loading={loading} data={configData} />
               <TransactionZoomChart />
             </div>
